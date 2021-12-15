803a8033b1826741a2dec49d0d8a19fdff0f78ac
HP <shubhi1488@gmail.com>
2021-12-15 21:20:16 +0530

    Exception handling

--- src/TestAge.java
+++ src/TestAge.java
@@ -1,6 +1,7 @@
 //in this we have implemented the concept of throw which allow us to create the custom
 // exceptions in this we have created the validate method in this if a person's age is less tha 18
 //than it will throw arithmatic exception and if it is larger than 18 than print eligible to vote.
+//we have created unchecked custom exceptions
 public class TestAge {
     public static void Validate(int age){
         if(age<18){
